3a11f9cddae6374248569e5a5bc68764c42e8af7
PoChangLin <allen4353968@gmail.com>
2020-10-04 23:52:59 +0800

    add cookie auth process

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('user');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user.id);
+    });
+});
+
 passport.use(
     new GoogleStrategy(
         {
@@ -17,6 +27,7 @@ passport.use(
                 googleId: profile.id
             }).then((existingUser) => {
                 if(existingUser){
+                    console.log(existingUser.id);
                     done(null, existingUser);
                 }else{
                     new User({
