bc065b061abb2d5f7220c154a454ca84d07f6565
PoChangLin <allen4353968@gmail.com>
2020-10-07 09:17:03 +0800

    client setup

--- services/passport.js
+++ services/passport.js
@@ -23,21 +23,19 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({
                 googleId: profile.id
-            }).then((existingUser) => {
-                if(existingUser){
-                    console.log(existingUser.id);
-                    done(null, existingUser);
-                }else{
-                    new User({
-                        googleId: profile.id
-                    })
-                    .save()
-                    .then(user => done(null, user));
-                }
             });
+            
+            if(existingUser){
+                return done(null, existingUser);
+            }
+            
+            const user = await new User({
+                googleId: profile.id
+            }).save();
+            done(null, user);
         }
     )
 );
