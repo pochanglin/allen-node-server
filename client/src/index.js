0b4d76416bd1fce20507fb4bbe9f5f1aa46868d9
PoChangLin <allen4353968@gmail.com>
2020-10-14 00:11:28 +0800

    adding auth actions

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'; 
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
 const store = createStore(
     reducers,
     {},
-    applyMiddleware()
+    applyMiddleware(reduxThunk)
 );
 
 ReactDOM.render(
